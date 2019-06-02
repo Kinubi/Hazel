29d725dc6eb7cbe5fdbfef5db2373c5429e6019d
Kinubi <barendbth@gmail.com>
2019-06-02 19:01:40 +0200

    Renderer API abstraction

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -9,6 +9,9 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Buffer.h"
+
 namespace Hazel {
 
 	class HAZEL_API Application
@@ -36,7 +39,10 @@ namespace Hazel {
 
 		static Application* s_Instance;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
+		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr <VertexBuffer> m_VertexBuffer;
+		std::unique_ptr <IndexBuffer> m_IndexBuffer;
 	};
 	// To be defined in client
 	Application* CreateApplication();
